a57ec33d178506f3d7510ff85d32b299fb5df46d
horizonshd <horizonshd@qq.com>
2024-06-21 22:03:58 +0800

    custom key maps

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -10,3 +10,8 @@ vim.keymap.set('i', '<A-j>', '<Esc>:m .+1<CR>==gi', { noremap = true, desc = 'Mo
 vim.keymap.set('i', '<A-k>', '<Esc>:m .-2<CR>==gi', { noremap = true, desc = 'Move line(s) down' })
 vim.keymap.set('v', '<A-j>', [[:m '>+1<CR>gv=gv]], { noremap = true, desc = 'Move line(s) up' })
 vim.keymap.set('v', '<A-k>', [[:m '<-2<CR>gv=gv]], { noremap = true, desc = 'Move line(s) down' })
+
+vim.keymap.set('n', '\\', ':NvimTreeToggle<CR>', { noremap = true, desc = 'Toggle NvimTree' })
+
+vim.keymap.set('n', '<Tab>', ':bn<CR>', { noremap = true, desc = 'Go to next buffer' })
+vim.keymap.set('n', '<S-Tab>', ':bp<CR>', { noremap = true, desc = 'Go to previous buffer' })
