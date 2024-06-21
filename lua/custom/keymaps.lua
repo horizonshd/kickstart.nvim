fad9c825f1c20eab5682547eb7bbd2e5785b2421
horizonshd <horizonshd@outlook.com>
2024-06-21 18:01:38 +0800

    custom keymaps for move line

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,3 +1,12 @@
-vim.keymap.set('i', 'jf', '<Esc>', { desc = 'Exit insert mode' })
-vim.keymap.set('c', 'jf', '<C-c>', { desc = 'Exit command-line mode' })
-vim.keymap.set('t', 'jf', [[<C-\><C-n>]], { desc = 'Exit terminal mode' })
+vim.keymap.set('i', 'jf', '<Esc>', { noremap = true, desc = 'Exit insert mode' })
+vim.keymap.set('c', 'jf', '<C-c>', { noremap = true, desc = 'Exit command-line mode' })
+vim.keymap.set('t', 'jf', [[<C-\><C-n>]], { noremap = true, desc = 'Exit terminal mode' })
+
+----> Move lines
+---- https://stackoverflow.com/questions/741814/move-entire-line-up-and-down-in-vim#answer-4121798
+vim.keymap.set('n', '<A-j>', ':m .+1<CR>==', { noremap = true, desc = 'Move line(s) up' })
+vim.keymap.set('n', '<A-k>', ':m .-2<CR>==', { noremap = true, desc = 'Move line(s) down' })
+vim.keymap.set('i', '<A-j>', '<Esc>:m .+1<CR>==gi', { noremap = true, desc = 'Move line(s) up' })
+vim.keymap.set('i', '<A-k>', '<Esc>:m .-2<CR>==gi', { noremap = true, desc = 'Move line(s) down' })
+vim.keymap.set('v', '<A-j>', [[:m '>+1<CR>gv=gv]], { noremap = true, desc = 'Move line(s) up' })
+vim.keymap.set('v', '<A-k>', [[:m '<-2<CR>gv=gv]], { noremap = true, desc = 'Move line(s) down' })
