c61fc8dc1ab67b95de7b2344127464dd762f64ba
horizonshd <horizonshd@outlook.com>
2024-06-21 17:14:27 +0800

    custom

--- init.lua
+++ init.lua
@@ -95,9 +95,11 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 require 'options'
+
+require 'custom.options'
 
 require 'keymaps'
 
