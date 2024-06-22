688d37d5752e494d46fd6013ef287ae9fb61234e
horizonshd <horizonshd@qq.com>
2024-06-22 09:53:35 +0800

    refactor custom keymaps

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,15 +1,24 @@
-vim.keymap.set('i', 'jf', '<Esc>', { noremap = true, desc = 'Exit insert mode' })
-vim.keymap.set('c', 'jf', '<C-c>', { noremap = true, desc = 'Exit command-line mode' })
-vim.keymap.set('t', 'jf', [[<C-\><C-n>]], { noremap = true, desc = 'Exit terminal mode' })
+local function map(mode, lhs, rhs, desc)
+  vim.keymap.set(mode, lhs, rhs, { noremap = true, desc = 'Horizonshd: ' .. desc })
+end
+
+map('i', 'jf', '<Esc>', 'Exit insert mode')
+map('c', 'jf', '<C-c>', 'Exit command-line mode')
+map('t', 'jf', [[<C-\><C-n>]], 'Exit terminal mode')
 
 ----> Move lines
 ---- https://stackoverflow.com/questions/741814/move-entire-line-up-and-down-in-vim#answer-4121798
-vim.keymap.set('n', '<A-j>', ':m .+1<CR>==', { noremap = true, desc = 'Move line(s) up' })
-vim.keymap.set('n', '<A-k>', ':m .-2<CR>==', { noremap = true, desc = 'Move line(s) down' })
-vim.keymap.set('i', '<A-j>', '<Esc>:m .+1<CR>==gi', { noremap = true, desc = 'Move line(s) up' })
-vim.keymap.set('i', '<A-k>', '<Esc>:m .-2<CR>==gi', { noremap = true, desc = 'Move line(s) down' })
-vim.keymap.set('v', '<A-j>', [[:m '>+1<CR>gv=gv]], { noremap = true, desc = 'Move line(s) up' })
-vim.keymap.set('v', '<A-k>', [[:m '<-2<CR>gv=gv]], { noremap = true, desc = 'Move line(s) down' })
+map('n', '<A-j>', ':m .+1<CR>==', 'Move line(s) up')
+map('n', '<A-k>', ':m .-2<CR>==', 'Move line(s) down')
+map('i', '<A-j>', '<Esc>:m .+1<CR>==gi', 'Move line(s) up')
+map('i', '<A-k>', '<Esc>:m .-2<CR>==gi', 'Move line(s) down')
+map('v', '<A-j>', [[:m '>+1<CR>gv=gv]], 'Move line(s) up')
+map('v', '<A-k>', [[:m '<-2<CR>gv=gv]], 'Move line(s) down')
 
-vim.keymap.set('n', '<Tab>', ':bn<CR>', { noremap = true, desc = 'Go to next buffer' })
-vim.keymap.set('n', '<S-Tab>', ':bp<CR>', { noremap = true, desc = 'Go to previous buffer' })
+map('n', '<Tab>', ':bn<CR>', 'Go to next buffer')
+map('n', '<S-Tab>', ':bp<CR>', 'Go to previous buffer')
+
+map('n', ';', ':', 'Enter command line mode')
+
+map({ 'n', 'v' }, '<S-j>', '10j', 'Move 10 lines down')
+map({ 'n', 'v' }, '<S-k>', '10k', 'Move 10 lines up')
