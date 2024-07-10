b23a6440702d7732cb5e883fc919370c69b1ca96
horizonshd <horizonshd@outlook.com>
2024-07-10 12:29:11 +0800

    switch default color scheme to gruvbox

--- lua/custom/plugins/monokai-pro.lua
+++ lua/custom/plugins/monokai-pro.lua
@@ -5,11 +5,11 @@ return {
     background_clear = { 'float_win' },
     transparent_background = true,
   },
-  init = function()
-    -- Load the colorscheme here.
-    vim.cmd.colorscheme 'monokai-pro-classic'
-
-    -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
-  end,
+  -- init = function()
+  --   -- Load the colorscheme here.
+  --   vim.cmd.colorscheme 'monokai-pro-classic'
+  --
+  --   -- You can configure highlights by doing something like:
+  --   vim.cmd.hi 'Comment gui=none'
+  -- end,
 }
